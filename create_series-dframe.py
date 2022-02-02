4bb1f550b089ad0b8d12f46f5d85da42c885eaaa
jojohahana <yohana.chriezt@gmail.com>
2022-02-02 07:53:00 +0700

    save modif series d-frame

---
+++ create_series-dframe.py
@@ -0,0 +1,6 @@
+# Creating Series & Dataframe from List
+# Untuk membuat Series atau Dataframe bisa dari berbagai macam tipe data container/mapping di python, 
+# seperti list dan dictionary, maupun dari numpy array.
+# Pada sub bagian ini, kamu akan membuat Series dan Dataframe yang bersumber dari list. 
+# Sekadar meninjau bahwa list merupakan sebuah kumpulan data berbagai macam tipe data yang mutable (dapat diganti)
+
